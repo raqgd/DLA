e8befefbfbf3f9529239c4d6d4fbbd0eeed5c457
Secreto_Angel <iradiactive@gmail.com>
2024-03-16 16:51:37 +0100

    Vista Jugadores y Mercado con Directivas

--- proyecto_angular/src/app/jugadores/jugadores.component.ts
+++ proyecto_angular/src/app/jugadores/jugadores.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-jugadores',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './jugadores.component.html',
   styleUrl: './jugadores.component.scss'
 })
 export class JugadoresComponent {
-
+  jugadors =[
+    {icono:'Alaves', equipo:'Almeria', nombre:"Nombre Apellido Apellido", valorActual:'400000', valorFuturo:'500000', puntos:'8'},
+    {icono:'Alaves', equipo:'Almeria', nombre:"Nombre Apellido Apellido", valorActual:'400000', valorFuturo:'500000', puntos:'8'},
+    {icono:'Alaves', equipo:'Almeria', nombre:"Nombre Apellido Apellido", valorActual:'400000', valorFuturo:'500000', puntos:'8'},
+    {icono:'Alaves', equipo:'Almeria', nombre:"Nombre Apellido Apellido", valorActual:'400000', valorFuturo:'500000', puntos:'8'},
+    {icono:'Alaves', equipo:'Almeria', nombre:"Nombre Apellido Apellido", valorActual:'400000', valorFuturo:'500000', puntos:'8'},
+    {icono:'Alaves', equipo:'Almeria', nombre:"Nombre Apellido Apellido", valorActual:'400000', valorFuturo:'500000', puntos:'8'}
+ ]
 }
--- proyecto_angular/src/app/mercado/mercado.component.ts
+++ proyecto_angular/src/app/mercado/mercado.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, NgModule } from '@angular/core';
 
 @Component({
   selector: 'app-mercado',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './mercado.component.html',
   styleUrl: './mercado.component.scss'
 })
 export class MercadoComponent {
-
+  jugadors =[
+     {icono:'Alaves', equipo:'Almeria', nombre:"Nombre Apellido Apellido", valorActual:'400000', valorFuturo:'500000', puntos:'8'},
+     {icono:'Alaves', equipo:'Almeria', nombre:"Nombre Apellido Apellido", valorActual:'400000', valorFuturo:'500000', puntos:'8'},
+     {icono:'Alaves', equipo:'Almeria', nombre:"Nombre Apellido Apellido", valorActual:'400000', valorFuturo:'500000', puntos:'8'},
+     {icono:'Alaves', equipo:'Almeria', nombre:"Nombre Apellido Apellido", valorActual:'400000', valorFuturo:'500000', puntos:'8'},
+     {icono:'Alaves', equipo:'Almeria', nombre:"Nombre Apellido Apellido", valorActual:'400000', valorFuturo:'500000', puntos:'8'},
+     {icono:'Alaves', equipo:'Almeria', nombre:"Nombre Apellido Apellido", valorActual:'400000', valorFuturo:'500000', puntos:'8'}
+  ]
 }
