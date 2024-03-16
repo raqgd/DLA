f38583fd8259f428e8ce5b12571d87142c189ddd
Secreto_Angel <iradiactive@gmail.com>
2024-03-16 18:02:08 +0100

    Mas directivas añadidas en jugadores y mercado

--- proyecto_angular/src/app/jugadores/jugadores.component.ts
+++ proyecto_angular/src/app/jugadores/jugadores.component.ts
@@ -9,12 +9,20 @@ import { CommonModule } from '@angular/common';
   styleUrl: './jugadores.component.scss'
 })
 export class JugadoresComponent {
+  cambiarEstadoJugador(id: number) {
+    const boton = this.jugadors.find(b => b.id === id);
+    if (boton) {
+      boton.estado = !boton.estado;
+    }
+  }
+
   jugadors =[
-    {icono:'Alaves', equipo:'Almeria', nombre:"Nombre Apellido Apellido", valorActual:'400000', valorFuturo:'500000', puntos:'8'},
-    {icono:'Alaves', equipo:'Almeria', nombre:"Nombre Apellido Apellido", valorActual:'400000', valorFuturo:'500000', puntos:'8'},
-    {icono:'Alaves', equipo:'Almeria', nombre:"Nombre Apellido Apellido", valorActual:'400000', valorFuturo:'500000', puntos:'8'},
-    {icono:'Alaves', equipo:'Almeria', nombre:"Nombre Apellido Apellido", valorActual:'400000', valorFuturo:'500000', puntos:'8'},
-    {icono:'Alaves', equipo:'Almeria', nombre:"Nombre Apellido Apellido", valorActual:'400000', valorFuturo:'500000', puntos:'8'},
-    {icono:'Alaves', equipo:'Almeria', nombre:"Nombre Apellido Apellido", valorActual:'400000', valorFuturo:'500000', puntos:'8'}
- ]
+     {id:1, icono:'Alaves', equipo:'Almeria', nombre:"Nombre Apellido Apellido", valorActual:'400000', valorFuturo:'500000', puntos:'8', estado: false},
+     {id:2, icono:'Alaves', equipo:'Almeria', nombre:"Nombre Apellido Apellido", valorActual:'400000', valorFuturo:'500000', puntos:'8', estado: false},
+     {id:3, icono:'Alaves', equipo:'Almeria', nombre:"Nombre Apellido Apellido", valorActual:'400000', valorFuturo:'500000', puntos:'8', estado: false},
+     {id:4, icono:'Alaves', equipo:'Almeria', nombre:"Nombre Apellido Apellido", valorActual:'400000', valorFuturo:'500000', puntos:'8', estado: false},
+     {id:5, icono:'Alaves', equipo:'Almeria', nombre:"Nombre Apellido Apellido", valorActual:'400000', valorFuturo:'500000', puntos:'8', estado: false},
+     {id:6, icono:'Alaves', equipo:'Almeria', nombre:"Nombre Apellido Apellido", valorActual:'400000', valorFuturo:'500000', puntos:'8', estado: false}
+  ];
+  valores: number[] = [3, 5, 8, 2, 10, 13];
 }
