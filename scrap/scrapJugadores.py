4052a2b9f7464792b5b31ad8666a238d621e376f
raqgd <99642900+raqgd@users.noreply.github.com>
2024-03-17 13:11:50 +0100

    Todos los csv y .py

--- scrap/scrapJugadores.py
+++ scrap/scrapJugadores.py
@@ -22,7 +22,7 @@ password = 'PC1_GR_ 4'
 #Abrir adblock
 options = webdriver.ChromeOptions()
 options.add_argument('--ignore-certificate-errors')
-options.add_argument('load-extension=' + r"C:\Users\Raque\OneDrive\Documentos\Uni\3º\1semestre\Proyecto I\webscrapping\adblocker")
+options.add_argument('load-extension=' + r"C:\Users\pelay\Desktop\PC1\DreamLeagueAssitant_PC1\adblocker")
 options.add_argument('--disable-notifications')
 driver = webdriver.Chrome(options=options)
 driver.maximize_window()
@@ -41,7 +41,7 @@ time.sleep(1)
 
 #Aceptar cookies
 wait = WebDriverWait(driver, 10)
-boton_cookies = driver.find_element(By.XPATH, '//*[@id="didomi-notice-agree-button"]')
+boton_cookies = driver.find_element(By.XPATH, '/html/body/div[1]/div/div/div/div/div/div[2]/button[2]')
 boton_cookies.click()
 
 time.sleep(0.2)
@@ -62,26 +62,38 @@ time.sleep(1)
 #Metemos el email
 email = driver.find_element(By.XPATH, '/html/body/div[2]/div/div/div[2]/div/form/div[1]/input')
 email.send_keys('dreamleagueassistant@gmail.com')
-time.sleep(0.5)
+time.sleep(1)
 
 #Metemos contraseña
 password = driver.find_element(By.XPATH, '/html/body/div[2]/div/div/div[2]/div/form/div[2]/input')
 password.send_keys('PC1_GR_ 4')
-time.sleep(0.5)
+time.sleep(1)
 
 #Le damos a iniciar sesion
 boton_iniciar = driver.find_element(By.XPATH, '/html/body/div[2]/div/div/div[2]/div/form/div[3]/button')
 boton_iniciar.click()
-time.sleep(.7)
+time.sleep(1)
 
 #Nos vamos a más
 boton_mas = driver.find_element(By.XPATH, '//*[@id="content"]/header/div[2]/ul/li[5]/a')
 boton_mas.click()
-time.sleep(1.3)
+time.sleep(.7)
 
 #Nos vamos a jugadores
-boton_jugadores = driver.find_element(By.XPATH, '//*[@id="content"]/div[4]/div[1]/button[2]')
+boton_jugadores = driver.find_element(By.XPATH, '//*[@id="content"]/div[2]/div[1]/button[2]')
 boton_jugadores.click()
+time.sleep(1)
+
+#Le damos al desplegable
+boton_2 = driver.find_element(By.XPATH, '/html/body/div[7]/div[3]/div[1]/div[3]/div/select')
+boton_2.click()
+time.sleep(1)
+
+opcion_2 = driver.find_element(By.XPATH, '/html/body/div[7]/div[3]/div[1]/div[3]/div/select/option[2]')
+opcion_2.click()
+time.sleep(1)
+
+boton_2.click()
 time.sleep(1)
 
 button = True
@@ -89,7 +101,7 @@ while button:
 
     try:
         driver.find_element(By.CLASS_NAME, 'search-players-more')
-        time.sleep(1)
+        time.sleep(3)
         driver.find_element(By.CLASS_NAME, 'search-players-more').click()
     except:
         button = False
@@ -109,28 +121,51 @@ lista_jugadores = driver.find_element(By.CLASS_NAME, 'player-list').find_element
 
 for jugador in lista_jugadores:
     link = jugador.find_element(By.TAG_NAME, 'a').get_attribute('href')
-    # regex_link = r"(https://mister\.mundodeportivo\.com)/players/(\d+)/(.+)"
-    # link = re.sub(regex_link, r"\1/more#players/\2/\3", link_unmod)
     links_jugadores.append(link)
 
 jugadores_info = []
-csv_columns = ['ID', 'Jornada', 'Puntos', 'Nombre', 'Equipo', 'Posición', 'Valoración', 'Pases totales', 'Pases precisos', 'Balones en largo totales', 'Centros totales', 'Duelos aéreos perdidos', 'Duelos perdidos', 'Duelos ganados', 'Regateado', 'Regates totales', 'Regates completados', 'Tiros a puerta', 'Tiros bloqueados en ataque', 'Goles', 'Despejes totales', 'Intercepciones', 'Entradas totales', 'Faltas recibidas', 'Minutos jugados', 'Toques', 'Valoración', 'Posesiones perdidas', 'Goles esperados', 'Pases clave', 'MATCH_STAT_expectedAssists', 'Balones en largo precisos', 'Asistencias de gol', 'Pérdidas', 'Tiros fuera', 'Faltas cometidas', 'Duelos aéreos ganados', 'Tiros bloqueados en defensa', 'Centros precisos', 'Ocasiones claras falladas', 'Ocasiones creadas', 'Fueras de juego', 'Errores que llevan a disparo', 'Tiros al palo', 'Penaltis provocados', 'Despejes en la línea de gol', 'Despejes por alto', 'Paradas desde dentro del área', 'Paradas', 'Salidas totales', 'Salidas precisas', 'Goles evitados', 'Despejes con los puños', 'Entradas como último hombre', 'Errores que llevan a gol', 'Penaltis parados', '', 'Penaltis cometidos', 'Penaltis fallados']
+csv_columns = ['ID', 'Nombre', 'Equipo', 'Posición']
 
-for i, link in enumerate(links_jugadores):
-    while i >= 21 and i <= 40:  # Si ya hemos procesado 20 enlaces, salimos del bucle
-        driver.get(link)
-        time.sleep(.3)
-        # Información jugadores
-        regex_id = r"/players/(\d+)/"
-        id = int(re.search(regex_id, link).group(1))
-        player_info = driver.find_element(By.CLASS_NAME, 'player-info').find_element(By.CLASS_NAME, 'left')
-        player_name = f'{driver.find_element(By.CLASS_NAME, "name").text.strip()} {driver.find_element(By.CLASS_NAME, "surname").text.strip()}' if driver.find_element(By.CLASS_NAME, "name").text.strip() else driver.find_element(By.CLASS_NAME, "surname").text.strip()
-        position_team = player_info.find_element(By.CLASS_NAME, 'team-position')
-        team = position_team.find_element(By.TAG_NAME, 'a').get_attribute('data-title')
-        posicion_numero = position_team.find_element(By.TAG_NAME, 'i').get_attribute('class')
-        numero_posicion = posicion_numero.split(' ')[0].split('-')[1]
-        posicion = posiciones.get(numero_posicion, "Desconocido")
+header_not_completed = True
 
-        caja_jornadas = driver.find_element(By.CLASS_NAME, 'box-scores').find_elements(By.CLASS_NAME, 'btn-player-gw')
-        jornadas = [c.find_element(By.CLASS_NAME, 'gw').text.strip() for c in reversed(caja_jornadas)] 
+for link in links_jugadores:
+    driver.get(link)
+    regex_id = r'/players/(\d+)'
+    id = int(re.search(regex_id, link).group(1))
+    player_info = driver.find_element(By.CLASS_NAME, 'player-info').find_element(By.CLASS_NAME, 'left')
+    player_name = f'{driver.find_element(By.CLASS_NAME, "name").text.strip()} {driver.find_element(By.CLASS_NAME, "surname").text.strip()}' if driver.find_element(By.CLASS_NAME, "name").text.strip() else driver.find_element(By.CLASS_NAME, "surname").text.strip()
+    position_team = player_info.find_element(By.CLASS_NAME, 'team-position')
+    team = position_team.find_element(By.TAG_NAME, 'a').get_attribute('data-title')
+    posicion_numero = position_team.find_element(By.TAG_NAME, 'i').get_attribute('class')
+    numero_posicion = posicion_numero.split(' ')[0].split('-')[1]
+    posicion = posiciones.get(numero_posicion, "Desconocido")
 
+    # Obtener las jornadas y escribir la información en el diccionario
+    jugador_data = {'ID': id, 'Nombre': player_name, 'Equipo': team, 'Posición': posicion}
+
+    caja_jornadas = driver.find_element(By.CLASS_NAME, 'box-scores').find_elements(By.CLASS_NAME, 'btn-player-gw')
+    jornadas = [c.find_element(By.CLASS_NAME, 'gw').text.strip() for c in reversed(caja_jornadas)]
+
+    if header_not_completed:
+        for c in reversed(caja_jornadas):
+            numero_jornada = c.find_element(By.CLASS_NAME, 'gw').text.strip()
+            csv_columns.append(numero_jornada)
+        header_not_completed = False
+
+    for i, c in enumerate(reversed(caja_jornadas)):
+        barra_jornada = c.find_element(By.CLASS_NAME, 'bar')
+        try:
+            puntos = barra_jornada.find_element(By.CLASS_NAME, 'score').text.strip()
+        except:
+            puntos = 0
+        jugador_data[jornadas[i]] = puntos
+
+    jugadores_info.append(jugador_data)
+
+# Escribir la información en un archivo CSV
+csv_file_path = 'jugadores_info.csv'
+with open(csv_file_path, 'w', newline='', encoding='utf-8') as csv_file:
+    writer = csv.DictWriter(csv_file, fieldnames=csv_columns)
+    writer.writeheader()
+    for jugador_info in jugadores_info:
+        writer.writerow(jugador_info)
