833d67622bcd7b77390231b1e7d2d1c318bbd0e1
raqgd <99642900+raqgd@users.noreply.github.com>
2024-04-19 12:57:43 +0200

    Slide login funcional de nuevo

--- proyecto_angular/src/app/login/login.component.ts
+++ proyecto_angular/src/app/login/login.component.ts
@@ -6,8 +6,25 @@ import { Component, OnInit  } from '@angular/core';
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
 
   constructor() { }
 
+  ngOnInit(): void {
+    const container = document.getElementById('container');
+    const registerBtn = document.getElementById('register');
+    const loginBtn = document.getElementById('login');
+
+    if (container && registerBtn && loginBtn) {
+      registerBtn.addEventListener('click', () => {
+        container.classList.add("active");
+      });
+
+      loginBtn.addEventListener('click', () => {
+        container.classList.remove("active");
+      });
+    } else {
+      console.error("One or more elements not found.");
+    }
+}
 }
