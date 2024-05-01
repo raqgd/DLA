4f63070fc6416875c5166f4e9d14464cc90736ee
raqgd <99642900+raqgd@users.noreply.github.com>
2024-05-01 13:14:04 +0200

    Cmabio de color de boton con scroll

--- proyecto_angular/src/app/navbar/navbar.component.ts
+++ proyecto_angular/src/app/navbar/navbar.component.ts
@@ -1,14 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
   imports: [
-    RouterModule
+    RouterModule,
+    CommonModule
   ],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.scss'
+  styleUrl: './navbar.component.scss',
 })
 export class NavbarComponent {
+
+  scrolled: boolean = false;
+
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    // Lógica para determinar si se ha hecho scroll
+    if (window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop > 100) {
+      this.scrolled = true;
+    } else if (this.scrolled && window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop < 10) {
+      this.scrolled = false;
+    }
+}
 }
