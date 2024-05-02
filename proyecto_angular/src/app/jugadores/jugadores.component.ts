7b3841162ffaac6ae637a3459daa6ec8833e3f9b
raqgd <99642900+raqgd@users.noreply.github.com>
2024-05-02 14:43:46 +0200

    Boton borrado de jugador

--- proyecto_angular/src/app/jugadores/jugadores.component.ts
+++ proyecto_angular/src/app/jugadores/jugadores.component.ts
@@ -9,12 +9,13 @@ import { CommonModule } from '@angular/common';
   styleUrl: './jugadores.component.scss'
 })
 export class JugadoresComponent {
-  cambiarEstadoJugador(id: number) {
-    const boton = this.jugadors.find(b => b.id === id);
-    if (boton) {
-      boton.estado = !boton.estado;
-    }
-  }
+  mostrarBotonCierre: boolean = false;
+  // cambiarEstadoJugador(id: number) {
+  //   const boton = this.jugadors.find(b => b.id === id);
+  //   if (boton) {
+  //     boton.estado = !boton.estado;
+  //   }
+  // }
 
   jugadors =[
      {id:1, class:'uno', icono:'Alaves', equipo:'Almeria', nombre:"Nombre Apellido Apellido", valorActual:'400000', valorFuturo:'500000', puntos:'8', estado: false},
@@ -31,18 +32,9 @@ export class JugadoresComponent {
   ];
 
   constructor(private renderer: Renderer2, private el: ElementRef) {}
-  // aplicarReglas1() {
-  //   const elementosUno = this.el.nativeElement.querySelectorAll('.uno'); elementosUno.forEach((elemento: HTMLElement) => {this.renderer.setStyle(elemento, 'top', '12.5%');this.renderer.setStyle(elemento, 'left', '40%');});
-  //   const elementosDos = this.el.nativeElement.querySelectorAll('.dos'); elementosDos.forEach((elemento: HTMLElement) => {this.renderer.setStyle(elemento, 'top', '12.5%');this.renderer.setStyle(elemento, 'left', '60%');});
-  //   const elementosTre = this.el.nativeElement.querySelectorAll('.tre'); elementosTre.forEach((elemento: HTMLElement) => {this.renderer.setStyle(elemento, 'top', '32.5%');this.renderer.setStyle(elemento, 'left', '20%');});
-  //   const elementosCua = this.el.nativeElement.querySelectorAll('.cua'); elementosCua.forEach((elemento: HTMLElement) => {this.renderer.setStyle(elemento, 'top', '32.5%');this.renderer.setStyle(elemento, 'left', '40%');});
-  //   const elementosCin = this.el.nativeElement.querySelectorAll('.cin'); elementosCin.forEach((elemento: HTMLElement) => {this.renderer.setStyle(elemento, 'top', '32.5%');this.renderer.setStyle(elemento, 'left', '60%');});
-  //   const elementosSei = this.el.nativeElement.querySelectorAll('.sei'); elementosSei.forEach((elemento: HTMLElement) => {this.renderer.setStyle(elemento, 'top', '32.5%');this.renderer.setStyle(elemento, 'left', '80%');});
-  //   const elementosSie = this.el.nativeElement.querySelectorAll('.sie'); elementosSie.forEach((elemento: HTMLElement) => {this.renderer.setStyle(elemento, 'top', '52.5%');this.renderer.setStyle(elemento, 'left', '20%');});
-  //   const elementosOch = this.el.nativeElement.querySelectorAll('.och'); elementosOch.forEach((elemento: HTMLElement) => {this.renderer.setStyle(elemento, 'top', '52.5%');this.renderer.setStyle(elemento, 'left', '40%');});
-  //   const elementosNue = this.el.nativeElement.querySelectorAll('.nue'); elementosNue.forEach((elemento: HTMLElement) => {this.renderer.setStyle(elemento, 'top', '52.5%');this.renderer.setStyle(elemento, 'left', '60%');});
-  //   const elementosDie = this.el.nativeElement.querySelectorAll('.die'); elementosDie.forEach((elemento: HTMLElement) => {this.renderer.setStyle(elemento, 'top', '52.5%');this.renderer.setStyle(elemento, 'left', '80%');});
-  // }
+  agregarClose() {
+    this.mostrarBotonCierre = true;
+  }
   // aplicarReglas2() {
   //   const elementosUno = this.el.nativeElement.querySelectorAll('.uno'); elementosUno.forEach((elemento: HTMLElement) => {this.renderer.setStyle(elemento, 'top', '12.5%');this.renderer.setStyle(elemento, 'left', '30%');});
   //   const elementosDos = this.el.nativeElement.querySelectorAll('.dos'); elementosDos.forEach((elemento: HTMLElement) => {this.renderer.setStyle(elemento, 'top', '12.5%');this.renderer.setStyle(elemento, 'left', '50%');});
