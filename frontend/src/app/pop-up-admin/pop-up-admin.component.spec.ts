94697096a52f67ad192660d1516d4838f7ac7751
lgruneirodem <lgruneirodem@gmail.com>
2024-07-01 01:53:59 +0200

    cambios en pop up

---
+++ frontend/src/app/pop-up-admin/pop-up-admin.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { PopUpAdminComponent } from './pop-up-admin.component';
+
+describe('PopUpAdminComponent', () => {
+  let component: PopUpAdminComponent;
+  let fixture: ComponentFixture<PopUpAdminComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [PopUpAdminComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(PopUpAdminComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
