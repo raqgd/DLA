0ba87a3a7ee5d0f0569eba7ab64188cd8b9f91a2
raqgd <99642900+raqgd@users.noreply.github.com>
2024-06-30 18:07:16 +0200

    importacion app.component.scss en componentes
    para poder importar variables globales

--- frontend/src/environment/environment.ts
+++ frontend/src/environment/environment.ts
@@ -2,9 +2,4 @@ export const environment = {
     production: false,
     rutaApi: "https://localhost.8000/api/",
     url: 'http://localhost:4200',
-    blanco: '#ffffff',
-    azulosc: '#020322', 
-    azulosc_semit: '#020322d5', //Azul oscuro semitransparente para el footer
-    dorado: '#b88639',
-    negro: '#000000'
   };
